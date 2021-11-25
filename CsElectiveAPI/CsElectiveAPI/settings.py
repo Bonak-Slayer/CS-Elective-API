bfe21fe3b0937031dbf7d7b543ec8f76b1ba9a6c
LordGeese <horusagust@gmail.com>
2021-11-25 18:58:31 +0800

    added necessary files

--- CsElectiveAPI/CsElectiveAPI/settings.py
+++ CsElectiveAPI/CsElectiveAPI/settings.py
@@ -31,6 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'systemAPI',
+    'rest_framework',
+    'corsheaders',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
